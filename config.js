02125b3a45655a579da2906a23c177314c24094d
cerulean85 <zhwan85@gmail.com>
2022-01-18 21:30:38 +0900

    Revert "Revert "UPDATE""
    This reverts commit c8749ffa17e4ffeae8dec903fe0eebe4c0680df3.

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    access: {
+        host: "localhost",
+        port: 3000,
+        db: {
+            host: "localhost",
+            user: "root",
+            pass: "12345678"
+        }
+    }
+}
+
---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: "어서오세요!",
+        message: 'Hello Vue!!!',
+        my_items: [
+            { id: 0, name: "web"},
+            { id: 1, name: "is"},
+            { id: 2, name: "free"}
+        ]
+    }
+});

c1ba847633c794dc269bbcb6446813ec17e4119b
cerulean85 <zhwan85@gmail.com>
2022-01-19 20:35:24 +0900

    vue.js 스터디 중

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+Vue.component("todo-item", {
+    props: ["todo"],
+    template: "<li>{{ todo.text }}</li>"
+})
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -7,6 +12,32 @@ var app = new Vue({
             { id: 0, name: "web"},
             { id: 1, name: "is"},
             { id: 2, name: "free"}
+        ],
+        isMan: true,
+        gender: 0,
+        age: 65,
+        groceryList: [
+            { id: 0, text: "Vegetables" },
+            { id: 1, text: "Cheese" },
+            { id: 2, text: "Whatever else humans are supposed to eat" },
         ]
+    },
+    methods: {
+        switchGender: function () {
+            this.isMan = !this.isMan
+        },
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 });
+
+
+// var data = { a: 1}
+// var vm = new Vue({
+//     data: data
+// })
+//
+// vm.a === data.a
+// vm.a = 2
+//

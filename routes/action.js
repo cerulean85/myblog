e431b7f15dcc1be204ff4a73b97c610f4d991ac6
cerulean85 <zhwan85@gmail.com>
2022-01-23 21:08:09 +0900

    UPDATE...
    진짜 개발 시작했당..
    블로그와 게시판을 만들거당.

--- routes/action.js
+++ routes/action.js
@@ -1,4 +1,5 @@
 const config = require("../config")
+const query = require("../modules/query")
 
 module.exports = {
 
@@ -16,5 +17,111 @@ module.exports = {
 
             res.send(`Hello Textom!! ${userId}, ${userPass}`)
         }
+    },
+
+    insert_article: function(req, res) {
+        const userNo      = req.body.user_no
+        const title       = req.body.title
+        const contents    = req.body.contents
+        const files       = req.body.files
+        const contentType = req.body.content_type
+        query.query_insert({
+            "name": "insert_article",
+            "sql": `INSERT INTO article(user_no, content_type, title, contents, files, created_at) 
+                                 VALUES(${userNo}, ${contentType}, '${title}', '${contents}', '${files}', NOW())`,
+            "emit": function (result) {
+                console.log(`RESULT: ${result}`)
+            },
+            "call_res": res,
+            "reverse": false,
+            "res_send": true,
+        });
+    },
+
+    update_article: function(req, res) {
+        const articleNo   = req.body.article_no
+        const userNo      = req.body.user_no
+        const title       = req.body.title
+        const contents    = req.body.contents
+        const files       = req.body.files
+        const contentType = req.body.content_type
+        query.query_update({
+            "name": "update_article",
+            "sql": `UPDATE article SET
+                        content_type = ${contentType},
+                        title = '${title}',
+                        contents = '${contents}',
+                        files = '${files}',
+                        updated_at = NOW()
+                    WHERE no = ${articleNo}`,
+            "emit": function (result) {
+                console.log(`RESULT: ${result}`)
+            },
+            "call_res": res,
+            "reverse": false,
+            "res_send": true,
+        });
+    },
+    delete_article: function(req, res) {
+        const articleNo   = req.body.article_no
+        query.query_update({
+            "name": "update_article",
+            "sql": `DELETE FROM article WHERE no = ${articleNo}`,
+            "emit": function (result) {
+                console.log(`RESULT: ${result}`)
+            },
+            "call_res": res,
+            "reverse": false,
+            "res_send": true,
+        });
+    },
+
+    insert_file: function(req, res) {
+        const name      = req.body.name
+        const url       = req.body.url
+        const order     = req.body.order
+        query.query_insert({
+            "name": "insert_file",
+            "sql": `INSERT INTO article(name, url, order, updated_at) VALUES(${name}, ${url}, ${order}, NOW())`,
+            "emit": function (result) {console.log(`RESULT: ${result}`)},
+            "call_res": res,
+            "reverse": false,
+            "res_send": true,
+        });
+    },
+
+    update_file: function(req, res) {
+        const fileNo    = req.body.no
+        const name      = req.body.name
+        const url       = req.body.url
+        const order     = req.body.order
+        query.query_update({
+            "name": "update_file",
+            "sql": `UPDATE article SET
+                        name  = ${name},
+                        url   = ${url},
+                        order = ${order},
+                        updated_at = NOW()
+                    WHERE no = ${fileNo}`,
+            "emit": function (result) {
+                console.log(`RESULT: ${result}`)
+            },
+            "call_res": res,
+            "reverse": false,
+            "res_send": true,
+        });
+    },
+    delete_file: function(req, res) {
+        const fileNo    = req.body.no
+        query.query_update({
+            "name": "delete_file",
+            "sql": `DELETE FROM file WHERE no = ${fileNo}`,
+            "emit": function (result) {
+                console.log(`RESULT: ${result}`)
+            },
+            "call_res": res,
+            "reverse": false,
+            "res_send": true,
+        });
     }
 }
--- routes/router.js
+++ routes/router.js
@@ -7,5 +7,8 @@ router.get('/', function (req, res, next) {
 })
 
 router.post('/textom/login', action.textom.login)
+router.post('/insert_article', action.insert_article)
+router.post('/update_article', action.update_article)
+router.post('/delete_article', action.delete_article)
 
 module.exports = router
